3adeec562fd3add2944a3a3dd19197b4b01170c4
Raúl Martínez <rmartineza@ieselgrao.org>
2020-05-01 11:43:49 +0200

    Funciona

---
+++ par.py
@@ -0,0 +1,11 @@
+print ("Pares e impares")
+num=int(input('Introduce un número entero (0 para terminar): '))
+
+while num!= 0:
+    if num%2 == 0:
+        print ("El",num,"es par")
+    else:
+        print ("El",num,"es impar")
+    num=int(input('Introduce algún número entero: '))
+
+print ("Gracias por aprender")
